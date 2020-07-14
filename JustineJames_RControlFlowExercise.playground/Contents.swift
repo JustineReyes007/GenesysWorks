a1ca4f46a711aa3065e4796d270f1f98a222db35
JustineReyes007 <67969003+JustineReyes007@users.noreply.github.com>
2020-07-14 11:19:15 -0500

    Add files via upload

---
+++ JustineJames_RControlFlowExercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+let Welcome = "This is our egg timer"
+
+for index in 1...6 {
+    print ("\(index) times 30 is \(index * 30)")
+}
+    
+//when timer stops
+var Timer = 180
+
+if Timer == 180 {
+    print ("Timer Stopped")
+} else {
+    print("Timer is on")
+}
+
+
+
